d289a536eebca48e2a38430ee9e75622c8261914
ashiato45 <katiiijp@gmail.com>
2020-05-19 23:30:11 +0900

    added analyzer

---
+++ webapp/go/see_access.py
@@ -0,0 +1,13 @@
+import json
+import pandas as pd
+import re
+
+j = pd.read_json("access.log", lines=True)
+j = j[j["msg"] == "Finish Access"]
+
+j["url"] = j["url"].map(lambda x: re.sub(r'([a-z]|\d)+\.([a-z]|\d)+', "X", x))
+
+g = j.groupby(["method", "url"])
+print(g.count().sort_values("level", ascending=False)[:20])
+
+print(g.mean().sort_values("elapsed", ascending=False)[:20])
